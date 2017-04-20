bc424da23385e29d0ed7326aa6ba63c71fb5933d
mayankiitg <gmayank43@gmail.com>
2017-04-20 19:53:16 +0530

    a

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ if __name__ == '__main__':
 
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
-    # app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
