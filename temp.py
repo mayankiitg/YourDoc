ba861fe1150b03d21ca72daef89b0de30ba4b0ee
mayankiitg <gmayank43@gmail.com>
2017-04-20 19:14:46 +0530

    Added Related symptoms functionality using cooccurence matrix

--- temp.py
+++ temp.py
@@ -1,16 +1,26 @@
+import csv
 
 
-f = open("symptoms.txt", 'rb')
-data = f.read()
 
-data = data.split(",")
+with open("nodetable.csv","r") as csvfile:
+    reader = csv.reader(csvfile)
+    symp = []
+    dise = []
+    for row in reader:
+        if row[2] == 'symptom':
+            symp.append(row[1])
+        if row[2] == 'disease':
+            dise.append(row[1])
 
-with open("allsymptoms.txt", 'wb') as f3:
-	for symptom in data:
-		symptom = symptom.strip()
-		f3.write(symptom + "\n")
+
+# with open("allsymptoms.txt", 'wb') as f3:
+# 	for symptom in data:
+# 		symptom = symptom.strip()
+# 		f3.write(symptom + "\n")
+
+
 
 with open("symptomsEntity.txt", 'wb') as f2:
-	for symptom in data:
+	for symptom in symp:
 		symptom = symptom.strip()
 		f2.write('"' + symptom + '"' + ',' + '"' + symptom + '"' + "\n")
