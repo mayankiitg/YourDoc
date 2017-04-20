958ea1fc1f04a9581ca88fe1c4b6f243d4137f8d
Kenil Tanna <avidkenil@gmail.com>
2017-04-20 19:40:42 +0530

    removed errors

--- app.py
+++ app.py
@@ -255,4 +255,4 @@ if __name__ == '__main__':
 
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
-    # app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
